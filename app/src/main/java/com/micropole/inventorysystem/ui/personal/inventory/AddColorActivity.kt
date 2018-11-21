82657e3759f62dbe69fc92d4d3b8c605212ce15a
xianzhong <huaixianzhong1314@163.com>
2018-11-21 17:45:09 +0800

    add color

---
+++ app/src/main/java/com/micropole/inventorysystem/ui/personal/inventory/AddColorActivity.kt
@@ -0,0 +1,21 @@
+package com.micropole.inventorysystem.ui.personal.inventory
+
+import com.xx.baseuilibrary.mvp.BaseMvpViewActivity
+
+/**
+ * @ClassName       AddColorActivity
+ * @Description     todo
+ * @Author          HuaiXianZhong
+ * @Sign            。。。
+ * @Date            2018/11/21 17:31
+ * @Copyright       Guangzhou micro pole mobile Internet Technology Co., Ltd.
+ */
+class AddColorActivity : BaseMvpViewActivity() {
+    override fun getActivityLayoutId(): Int = 0
+
+    override fun initData() {
+    }
+
+    override fun initEvent() {
+    }
+}
